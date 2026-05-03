d68050e839e6e71330f812c08541d500546d3f73
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-05-03 18:15:43 +0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: './postcss.config.js',
+    lightningcss: {
+      targets: browserslistToTargets(browserslist('defaults, Chrome >= 64, iOS >= 12, Safari >= 12, Android >= 4.4')),
+    },
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    cssMinify: 'lightningcss',
+    target: ['es2015', 'chrome69', 'ios12'],
   },
 });
